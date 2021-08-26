0422533d98a9dc7af12f8c894fafab7c0ff85bec
MattWoelk <matt.woelk@gmail.com>
2021-08-25 22:02:22 -0500

    spawn a spray of bullets at the player's location

--- game_plugin/src/player.rs
+++ game_plugin/src/player.rs
@@ -6,7 +6,11 @@ use bevy::prelude::*;
 pub struct PlayerPlugin;
 
 pub struct Player;
-pub struct Laser;
+
+#[derive(Default)]
+pub struct Laser {
+    pub direction: Vec3,
+}
 
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
@@ -82,22 +86,29 @@ fn shoot_bullet(
         let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
         for (transform, _) in query.iter_mut() {
-            commands
-                .spawn()
-                .insert_bundle(SpriteSheetBundle {
-                    texture_atlas: texture_atlas_handle.clone(),
-                    transform: transform.clone(), // Transform::from_translation(Vec3::new(0., 0., 1.)),
-                    sprite: TextureAtlasSprite::new(188),
-                    ..Default::default()
-                })
-                .insert(Laser);
+            let bullet_spread_directions = [
+                Vec3::new(0.5, 0.5, 0.),
+                Vec3::new(0.0, 1., 0.),
+                Vec3::new(-0.5, 0.5, 0.),
+            ];
+            for a in bullet_spread_directions {
+                commands
+                    .spawn()
+                    .insert_bundle(SpriteSheetBundle {
+                        texture_atlas: texture_atlas_handle.clone(),
+                        transform: transform.clone(),
+                        sprite: TextureAtlasSprite::new(188),
+                        ..Default::default()
+                    })
+                    .insert(Laser { direction: a });
+            }
         }
     }
 }
 
-fn laser_movement(mut commands: Commands, mut query: Query<(Entity, &mut Transform, With<Laser>)>) {
-    for (entity, mut transform, _) in query.iter_mut() {
-        transform.translation += Vec3::new(0., 4., 0.);
+fn laser_movement(mut commands: Commands, mut query: Query<(Entity, &mut Transform, &Laser)>) {
+    for (entity, mut transform, laser) in query.iter_mut() {
+        transform.translation += laser.direction * 8.; // Vec3::new(0., 4., 0.);
 
         if transform.translation.y > 280. {
             commands.entity(entity).despawn();
